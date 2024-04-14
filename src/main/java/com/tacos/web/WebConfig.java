d1a8b6c3bddc21e9f05432a9768882f25c9e00fc
leandre <tuyambazeleandre@gmail.com>
2024-04-15 00:03:09 +0200

    fix the webconfig class

--- src/main/java/com/tacos/web/WebConfig.java
+++ src/main/java/com/tacos/web/WebConfig.java
@@ -5,9 +5,10 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig  implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
-    public void AddViewControllers(ViewControllerRegistry registry) {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
     }
 }
