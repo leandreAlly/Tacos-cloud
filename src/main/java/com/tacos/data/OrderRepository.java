57f04d1c96c079e0fb109b3d3c845f8bcdd9d410
leandre <tuyambazeleandre@gmail.com>
2024-04-30 00:00:42 +0200

    add jpa support

--- src/main/java/com/tacos/data/OrderRepository.java
+++ src/main/java/com/tacos/data/OrderRepository.java
@@ -1,7 +1,8 @@
 package com.tacos.data;
 
 import com.tacos.Order;
+import org.springframework.data.repository.CrudRepository;
 
-public interface OrderRepository {
-    Order save(Order order);
+public interface OrderRepository  extends
+        CrudRepository<Order, Long> {
 }
