b729e81a982b4c0ce824f301106fd1b0ed48ca59
leandre <tuyambazeleandre@gmail.com>
2024-05-14 13:16:31 +0200

    Working with cassandra database

--- src/main/java/com/tacos/data/OrderRepository.java
+++ src/main/java/com/tacos/data/OrderRepository.java
@@ -3,6 +3,8 @@ package com.tacos.data;
 import com.tacos.Order;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.UUID;
+
 public interface OrderRepository  extends
-        CrudRepository<Order, Long> {
+        CrudRepository<Order, UUID> {
 }
